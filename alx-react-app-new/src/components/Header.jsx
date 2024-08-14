335cc1e12cf07ceea69eb91a67d2af28ea3778d0
Davies Gotosa <daviesgotosa10@gmail.com>
2024-08-14 13:50:47 +0200

    mpoe

--- alx-react-app-new/src/components/Header.jsx
+++ alx-react-app-new/src/components/Header.jsx
@@ -2,21 +2,10 @@
 
 function Header() {
   return (
-    <header style={{
-      background: 'linear-gradient(to left, blue , purple )',
-      color: 'white',
-      textAlign: 'center',
-      width: '100%',
-      padding: '10px' ,
-      borderRadius: '5px',
-      border: 'none'
-    }}>
-      <h1 style={{
-        color: 'white',
-        fontSize: '30px',
-        fontWeight: '400'
-      }}>My Favorite Cities</h1>
-    </header>
+    <header style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center' }}>
+    <h1>My Favorite Cities</h1>
+  </header>
+  
   );
 }
 
