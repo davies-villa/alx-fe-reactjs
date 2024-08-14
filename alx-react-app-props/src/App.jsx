0752ae7e732b347df050cf4269c138dfbbecd2d0
Davies Gotosa <daviesgotosa10@gmail.com>
2024-08-14 20:12:38 +0200

    done

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,6 +1,5 @@
-import React from 'react';
-import ProfilePage from './ProfilePage';
-import UserContext from './UserContext';
+import UserContext from "./components/UserContext";
+import ProfilePage from "./components/ProfilePage";
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
--- alx-react-app-props/src/components/UserDetails.jsx
+++ alx-react-app-props/src/components/UserDetails.jsx
@@ -1,11 +1,11 @@
-import React, { useContext } from 'react';
+import { useContext } from 'react';
 import UserContext from './UserContext';
 
 function UserDetails() {
   const userData = useContext(UserContext);
 
   return (
-    <div className="card">
+    <div>
       <p>Name: {userData.name}</p>
       <p>Email: {userData.email}</p>
     </div>
