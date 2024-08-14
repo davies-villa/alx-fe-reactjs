ef2290660b0673786fed3f4135985b8119cb2805
Davies Gotosa <daviesgotosa10@gmail.com>
2024-08-14 18:36:42 +0200

    bhoo

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import ProfilePage from './ProfilePage';
 import UserContext from './UserContext';
-import './App.css';
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
   return (
-    <div className="container">
-      <UserContext.Provider value={userData}>
-        <ProfilePage />
-      </UserContext.Provider>
-    </div>
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
   );
 }
 
--- alx-react-app-props/src/UserDetails.jsx
+++ alx-react-app-props/src/UserDetails.jsx
@@ -5,7 +5,7 @@ function UserDetails() {
   const userData = useContext(UserContext);
 
   return (
-    <div>
+    <div className="card">
       <p>Name: {userData.name}</p>
       <p>Email: {userData.email}</p>
     </div>
