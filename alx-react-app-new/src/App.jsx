9f3cd328806329a03f0efe4062c2f98efa735ea6
Davies Gotosa <daviesgotosa10@gmail.com>
2024-08-14 13:33:57 +0200

    good

---
+++ alx-react-app-new/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Header />
+      <WelcomeMessage />
+      <MainContent />
+
+     
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+
+      <Footer />
+    </>
+  )
+}
+
+export default App
