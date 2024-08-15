681317418f9d0cf391d360c5f61532e451cf5a71
Davies Gotosa <daviesgotosa10@gmail.com>
2024-08-15 15:58:00 +0200

    didid

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav style={{ padding: '10px', textAlign: 'center', backgroundColor: '#333', color: '#fff' }}>
+    <nav style={{ padding: '10px', textAlign: 'center', backgroundColor: '#333', color: '#fff', display: 'flex', justifyContent: 'space-between' }}>
       <Link to="/" style={{ margin: '0 15px', color: '#fff', textDecoration: 'none' }}>Home</Link>
       <Link to="/about" style={{ margin: '0 15px', color: '#fff', textDecoration: 'none' }}>About</Link>
       <Link to="/services" style={{ margin: '0 15px', color: '#fff', textDecoration: 'none' }}>Services</Link>
