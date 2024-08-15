b1c10dbd0a99030807db5082e0e86e46095a9944
Davies Gotosa <daviesgotosa10@gmail.com>
2024-08-15 15:34:10 +0200

    bhoo

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './components/Home';
+import Navbar from './components/Navbar';
 import About from './components/About';
 import Services from './components/Services';
 import Contact from './components/Contact';
@@ -8,13 +9,8 @@ import Footer from './components/Footer';
 function App() {
   return (
     <Router>
+      <Navbar />
       <div>
-        <nav style={{ padding: '10px', backgroundColor: '#282c34', color: 'white' }}>
-          <Link to="/" style={{ margin: '0 10px', color: 'white' }}>Home</Link>
-          <Link to="/about" style={{ margin: '0 10px', color: 'white' }}>About</Link>
-          <Link to="/services" style={{ margin: '0 10px', color: 'white' }}>Services</Link>
-          <Link to="/contact" style={{ margin: '0 10px', color: 'white' }}>Contact</Link>
-        </nav>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
