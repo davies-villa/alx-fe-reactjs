2ec9df152039173b155aa4a599a51dea3a0798c9
Davies Gotosa <daviesgotosa10@gmail.com>
2024-09-02 17:19:03 +0200

    did

--- react-todo/src/__tests__/TodoList.test.js
+++ react-todo/src/__tests__/TodoList.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
-import TodoList from '../TodoList';
+import TodoList from '../components/TodoList';
 
 describe('TodoList Component', () => {
   test('renders TodoList component correctly', () => {
