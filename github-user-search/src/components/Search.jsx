29af068c562cccb66d04abfbc2d59ea0e45c9a1d
Davies Gotosa <daviesgotosa10@gmail.com>
2024-09-21 14:33:22 +0200

    added more items include the "Looks like we cant find the user"

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -21,21 +21,21 @@ const Search = () => {
             const data = await fetchUserData(username);
             setUserData(data);
         } catch (error) {
-            setError('Looks like we can’t find the user');
+            setError('Looks like we cant find the user');
         } finally {
             setLoading(false);
         }
     };
 
     return (
-        <div className="search-container">
-            <form onSubmit={handleFormSubmit} className="flex flex-col items-center gap-4">
+        <div className="search-container h-screen">
+            <form onSubmit={handleFormSubmit} className="w-full flex flex-col items-center justify-center gap-4">
                 <input
                     type="text"
                     placeholder="Search GitHub Username"
                     value={username}
                     onChange={handleInputChange}
-                    className="p-2 border border-gray-300 rounded"
+                    className="p-2 border border-blue-300 rounded"
                 />
                 <button
                     type="submit"
@@ -49,8 +49,8 @@ const Search = () => {
             {loading && <p>Loading...</p>}
             {error && <p>{error}</p>}
             {userData && (
-                <div className="user-details">
-                    <img src={userData.avatar_url} alt={userData.login} className="avatar flex items-center justify-center" />
+                <div className="user-details lex flex-col items-center justify-center">
+                    <img src={userData.avatar_url} alt={userData.login} className="avatar " />
                     <h2>{userData.name}</h2>
                     <p>
                         <a href={userData.html_url} target="_blank" rel="noopener noreferrer">
